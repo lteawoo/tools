af6e815a65c231cb2406a80a1c416d9a89e88495
twlee <twlee@nexon.co.kr>
2023-01-02 19:04:31 +0900

    area calculator update

---
+++ components/base/input/inputSelect.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-text-field
+    v-model="localInput"
+    variant="outlined"
+    hide-details
+    @input="$emit('input', $event)"
+  />
+  <v-select
+    v-model="localSelect"
+    :items="items"
+    item-title="label"
+    item-value="value"
+    density="compact"
+    variant="outlined"
+    hide-details
+  />
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  items?: Array<Object>,
+  input: string,
+  select: string
+}>(), {
+  items: () => []
+})
+
+const emit = defineEmits<{
+(event: 'update:input', payload: string): void
+(event: 'update:select', payload: string): void
+(event: 'input', payload:InputEvent): void
+}>()
+
+const localInput = computed({
+  get () {
+    return props.input
+  },
+
+  set (n) {
+    emit('update:input', n)
+  }
+})
+
+const localSelect = computed({
+  get () {
+    return props.select
+  },
+
+  set (n) {
+    emit('update:select', n)
+  }
+})
+</script>
+
+<style scoped>
+</style>
--- pages/calc/area.vue
+++ pages/calc/area.vue
@@ -7,14 +7,19 @@
       <v-card-text>
         <v-row>
           <v-col cols="6">
-            <v-text-field
-              prefix="제곱미터"
-              placeholder="test"
+            <input-select
+              v-model:input="input1"
+              v-model:select="selected1"
+              :items="selectItems1"
+              @input="calcInput1"
             />
           </v-col>
           <v-col cols="6">
-            <v-text-field
-              prefix="평"
+            <input-select
+              v-model:input="input2"
+              v-model:select="selected2"
+              :items="selectItems2"
+              @input="calcInput2"
             />
           </v-col>
         </v-row>
@@ -24,5 +29,32 @@
 </template>
 
 <script setup lang="ts">
+import InputSelect from '@/components/base/input/inputSelect.vue'
 
+const selectItems1 = [{
+  label: '평',
+  value: 'pyeong'
+}]
+const selectItems2 = [{
+  label: '제곱미터',
+  value: 'squareMeter'
+}]
+
+const input1 = ref<string>('1')
+const input2 = ref<string>('3.306')
+const selected1 = ref<string>('pyeong')
+const selected2 = ref<string>('squareMeter')
+
+onMounted(() => {
+})
+
+const calcInput1 = (e: InputEvent) => {
+  const target = <HTMLInputElement> e.target
+  input2.value = target.value ? String(Number(target.value) * 3.305785) : ''
+}
+
+const calcInput2 = (e: InputEvent) => {
+  const target = <HTMLInputElement> e.target
+  input1.value = target.value ? String(Number(target.value) / 3.305785) : ''
+}
 </script>
